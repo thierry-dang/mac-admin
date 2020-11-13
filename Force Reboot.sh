a802a7df5df521f78f8e0e06a63cf24a778f95ac
DlyanFR <47043212+DlyanFR@users.noreply.github.com>
2020-11-13 14:03:31 +0100

    Policy that force the system to reboot
    Open Jamf Pro console -> "Settings" -> "Computer Management" -> "Scripts"
    Click New -> Script.
    Copy / Paste the code and Save.
    Create your Policy, select Scripts -> Configure and select your script.
    Make the policy available in Self-Service.

---
+++ Force Reboot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+declare -a killPIDs
+killPIDs=$(ps axww -o pid,command | grep -v bash | grep [A]pplications/ | grep -v /bin/sh | grep -v [C]asper | grep -v [J]amf | grep -v [S]elf\ Service | grep -v [M]cAfee | grep -v [V]ZAccess* | grep -v grep | awk '{print $1}')
+Kill said processes.
+
+
+for i in ${killPIDs[@]}
+	do
+    	echo "Killing PID $i"
+    	kill -9 $i
+done
+
+nvram -c
+shutdown -r now
